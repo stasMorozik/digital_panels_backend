a534e77592ffd0ce15d94671b2ec63ba79bad52a
Stas Morozov <stanim857@gmail.com>
2023-10-19 22:18:03 +0300

    postgresql adapters: playlist domain

--- apps/postgresql_adapters/lib/playlist/getting_list.ex
+++ apps/postgresql_adapters/lib/playlist/getting_list.ex
@@ -1,4 +1,4 @@
-defmodule PostgresqlAdapters.Playlist.Inserting do
+defmodule PostgresqlAdapters.Playlist.GettingList do
   alias Core.Playlist.Ports.GetterList
 
   alias Core.Playlist.Entity, as: PlaylistEntity
@@ -7,17 +7,21 @@ defmodule PostgresqlAdapters.Playlist.Inserting do
 
   alias Core.Shared.Types.Success
   alias Core.Shared.Types.Error
+  
+  alias Core.Playlist.Types.Filter
+  alias Core.Playlist.Types.Sort
+  alias Core.Shared.Types.Pagination
 
   @behaviour GetterList
 
   @impl GetterList
   def get(%Filter{
     user_id: filter_by_user_id,
-    name: filter_by_user_name,
-    created_f: filter_by_user_created_f,
-    created_t: filter_by_user_created_t,
-    updated_f: filter_by_user_updated_f,
-    updated_t: filter_by_user_updated_t
+    name: filter_by_name,
+    created_f: filter_by_created_f,
+    created_t: filter_by_created_t,
+    updated_f: filter_by_updated_f,
+    updated_t: filter_by_updated_t
   }, %Sort{
     name: sort_by_name,
     created: sort_by_created,
@@ -29,23 +33,48 @@ defmodule PostgresqlAdapters.Playlist.Inserting do
     case :ets.lookup(:connections, "postgresql") do
       [{"postgresql", "", connection}] ->
         
-        offset = cond do
-          page == 1 -> 0
-          page > 1 -> limit * (page-1)
-        end
-
-        query(%Filter{
+        filter = %Filter{
           user_id: filter_by_user_id,
-          name: filter_by_user_name,
-          created_f: filter_by_user_created_f,
-          created_t: filter_by_user_created_t,
-          updated_f: filter_by_user_updated_f,
-          updated_t: filter_by_user_updated_t
-        },%Sort{
+          name: filter_by_name,
+          created_f: filter_by_created_f,
+          created_t: filter_by_created_t,
+          updated_f: filter_by_updated_f,
+          updated_t: filter_by_updated_t
+        }
+
+        sort = %Sort{
           name: sort_by_name,
           created: sort_by_created,
           updated: sort_by_updated
-        }, limit, offset)
+        }
+
+        pagi = %Pagination{
+          page: page,
+          limit: limit
+        }
+
+        # "
+        #   SELECT 
+        #     pl.id, pl.name, pl.contents, pl.created, pl.updated
+        #   FROM 
+        #     relations_user_playlist AS rl_u_p
+        #   JOIN 
+        #     playlists AS pl ON pl.id = rl_u_p.playlist_id
+        #   WHERE 
+        #     rl_u_p.user_id = $1
+        #   #{query_map.filter_by_name}
+        #   #{query_map.filter_by_created_f}
+        #   #{query_map.filter_by_created_t}
+        #   #{query_map.filter_by_updated_f}
+        #   #{query_map.filter_by_updated_t}
+        #   #{query_map.sort_by_name}
+        #   #{query_map.sort_by_created}
+        #   #{query_map.sort_by_updated}
+        #   LIMIT $2 OFFSET $3
+        # "
+
+        Success.new(true)
+
 
       [] -> Error.new("Database connection error")
       _ -> Error.new("Database connection error")
@@ -54,76 +83,5 @@ defmodule PostgresqlAdapters.Playlist.Inserting do
 
   def get(_, _, _) do
     Error.new("Не валидные данные для получения списка плэйлистов")
-  end
-
-  defp make_query_map(filter, sort) do
-    query_map = %{
-      counter: 3
-    }
-
-    with <- do
-    end
-
-    # if Map.has_key?(filter, :name) do
-    #   query_map = Map.put(filter, :filter_by_user_name, filter.name)
-    # end
-  end
-
-  defp query(filter, sort, limit, offset, connection) do
-
-    Map.put_new_lazy(query_map, :b, fun)
-    
-    # where_name = Map.get_lazy(filter, :name, fun = fn -> 
-    #   counter = counter + 1
-
-    #   "AND name LIKE $#{counter}"
-    # end)
-
-    # Map.put(filter_map, :name, )
-
-    # where = cond do
-    #   filter_by_user_name != nil -> 
-    #     where_count = where_count + 1
-        
-    #     "AND name LIKE $#{where_count}"
-    #   filter_by_user_name == nil -> ""
-    # end
-
-    # where = cond do
-    #   filter_by_user_created_f != nil && where_count ==  -> "AND name LIKE $4"
-    #   created == nil -> ""
-    # end
-
-    # text_query = "
-    #   SELECT 
-    #     pl.id, pl.name, pl.contents, pl.created, pl.updated
-    #   FROM 
-    #     relations_user_playlist AS rl_u_p
-    #   JOIN 
-    #     playlists AS pl ON pl.id = rl_u_p.playlist_id
-    #   WHERE 
-    #     rl_u_p.user_id = $1
-
-    #   LIMIT $2 OFFSET $3
-    # "
-
-    # query = Postgrex.prepare!(
-    #   connection,
-    #   "",
-    #   "
-    #     SELECT 
-    #       pl.id, pl.name, pl.contents, pl.created, pl.updated
-    #     FROM 
-    #       relations_user_playlist AS rl_u_p
-    #     JOIN 
-    #       playlists AS pl ON pl.id = rl_u_p.playlist_id
-    #     WHERE 
-    #       rl_u_p.user_id = $1
-    #     LIMIT $2 OFFSET $3
-    #   "
-    # )
-
-    # Postgrex.execute(connection, query, [user_id, limit, offset])
-    
   end
 end
---
+++ apps/postgresql_adapters/test/playlist/getting_list_test.exs
@@ -0,0 +1,88 @@
+defmodule Playlist.GettingLIstTest do
+  use ExUnit.Case
+  
+  alias PostgresqlAdapters.User.Inserting, as: UserInserting
+  alias PostgresqlAdapters.Playlist.Inserting, as: PlaylistInserting
+
+  alias Core.User.Builder, as: UserBuilder
+  alias Core.Playlist.Builder, as: PlaylistBuilder
+
+  alias PostgresqlAdapters.Playlist.GettingList
+
+  alias Core.Playlist.Types.Filter
+  alias Core.Playlist.Types.Sort
+  alias Core.Shared.Types.Pagination
+
+  doctest GettingList
+
+  setup_all do
+    File.touch("/tmp/not_emty.txt", 1544519753)
+    
+    File.write("/tmp/not_emty.txt", "content")
+
+    :ets.new(:connections, [:set, :public, :named_table])
+
+    {:ok, pid} = Postgrex.start_link(
+      hostname: "192.168.0.161",
+      username: "db_user",
+      password: "12345",
+      database: "system_content_manager",
+      port: 5437
+    )
+
+    :ets.insert(:connections, {"postgresql", "", pid})
+
+    Postgrex.query!(pid, "DELETE FROM relations_user_playlist", [])
+    Postgrex.query!(pid, "DELETE FROM relations_user_device", [])
+    Postgrex.query!(pid, "DELETE FROM relations_playlist_device", [])
+
+    Postgrex.query!(pid, "DELETE FROM playlists", [])
+    Postgrex.query!(pid, "DELETE FROM devices", [])
+    Postgrex.query!(pid, "DELETE FROM users WHERE email != 'stasmoriniv@gmail.com'", [])
+
+    :ok
+  end
+
+  test "Get" do
+    {:ok, user_entity} = UserBuilder.build(%{
+      email: "test@gmail.com", 
+      name: "Пётр", 
+      surname: "Павел"
+    })
+
+    {_, playlist_entity} = PlaylistBuilder.build(%{
+      name: "test",
+      contents: [
+        %{
+          file: %{
+            path: "/tmp/not_emty.txt"
+          },
+          display_duration: 15
+        }
+      ],
+      web_dav_url: "http://localhost"
+    })
+
+    {_, _} = UserInserting.transform(user_entity)
+
+    {_, _} = PlaylistInserting.transform(playlist_entity, user_entity)
+
+    {result, _} = GettingList.get(%Filter{
+      user_id: user_entity.id,
+      name: nil,
+      created_f: nil,
+      created_t: nil,
+      updated_f: nil,
+      updated_t: nil
+    }, %Sort{
+      name: nil,
+      created: nil,
+      updated: nil
+    }, %Pagination{
+      page: 1,
+      limit: 10
+    })
+
+    assert result == :ok
+  end
+end
