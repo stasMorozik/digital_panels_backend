88c323660e6731339c41fbc5c7e2a294c579c655
stasMorozik <103366332+stasMorozik@users.noreply.github.com>
2023-11-19 22:27:44 +0300

    Update updating_confirmed.ex

--- apps/postgresql_adapters/lib/confirmation_code/updating_confirmed.ex
+++ apps/postgresql_adapters/lib/confirmation_code/updating_confirmed.ex
@@ -9,18 +9,13 @@ defmodule PostgresqlAdapters.ConfirmationCode.UpdatingConfirmed do
   @behaviour Transformer
 
   @impl Transformer
-  def transform(%Entity{
-    needle: needle,
-    created: _,
-    code: _,
-    confirmed: true
-  }) do
+  def transform(%Entity{} = entity) do
     case :ets.lookup(:connections, "postgresql") do
       [{"postgresql", "", connection}] ->
 
         with query <- "UPDATE confirmation_codes SET confirmed = true WHERE needle = $1",
              {:ok, q} <- Postgrex.prepare(connection, "", query),
-             {:ok, _, _} <- Postgrex.execute(connection, q, [needle]) do
+             {:ok, _, _} <- Postgrex.execute(connection, q, [entity.needle]) do
           Success.new(true)
         else
           {:error, e} -> Exception.new(e.message)
