2438a6bc2a0096396643ac17d0e4b83f645a217a
stas <stas@debian.debian>
2024-01-10 16:01:24 +0300

    fixed mistakes

--- apps/core/lib/schedule/builders/hour.ex
+++ apps/core/lib/schedule/builders/hour.ex
@@ -4,14 +4,21 @@ defmodule Core.Schedule.Builders.Hour do
   alias Core.Shared.Types.Error
 
   @spec build(Success.t() | Error.t(), any()) :: Success.t() | Error.t()
-  def build({:ok, entity}, start_hour) do
-    case Core.Schedule.Validators.Hour.valid(start_hour) do
-      {:ok, _} -> {:ok, Map.put(entity, :start_hour, start_hour)}
+  def build({:ok, entity}, {:start, hour}) do
+    case Core.Schedule.Validators.Hour.valid(hour) do
+      {:ok, _} -> {:ok, Map.put(entity, :start_hour, hour)}
       {:error, message} -> {:error, message}
     end
   end
 
-  def build({:error, message}, _) do
+  def build({:ok, entity}, {:end, hour}) do
+    case Core.Schedule.Validators.Hour.valid(hour) do
+      {:ok, _} -> {:ok, Map.put(entity, :end_hour, hour)}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def build({:error, message}, _, _) do
     {:error, message}
   end
 end
