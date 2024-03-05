5504aa6192790827bedc2f8a6fd16f733fc12848
stas <stas@debian.debian>
2024-03-05 23:23:10 +0300

    http_adapters: file domain

--- apps/http_adapters/config/config.exs
+++ apps/http_adapters/config/config.exs
@@ -1,6 +1,5 @@
 import Config
 
-config :http_adapters, 
-  url: "http://192.168.0.106:8100/upload",
-  user: "user",
-  password: "12345"
+config :http_adapters,
+  user_web_dav: "user",
+  password_web_dav: "12345"
