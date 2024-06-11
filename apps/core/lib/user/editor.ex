caa3c5c20a03d319ebabe549dbef04bfada9849f
stas <stas@debian.debian>
2024-06-11 22:19:11 +0300

    fixed

--- apps/core/lib/user/editor.ex
+++ apps/core/lib/user/editor.ex
@@ -7,6 +7,11 @@ defmodule Core.User.Editor do
   alias Core.Shared.Types.Error
   alias Core.User.Entity
 
+  alias Core.Shared.Builders.BuilderProperties
+
+  alias Core.Shared.Validators.Email
+  alias Core.User.Validators.Name
+
   @spec edit(Entity.t(), map()) :: Success.t() | Error.t()
   def edit(%Entity{} = entity, args) when is_map(args) do
     entity(entity)
@@ -33,7 +38,7 @@ defmodule Core.User.Editor do
   defp email({:ok, entity}, email) do
     case email do
       nil -> {:ok, entity}
-      email -> Core.User.Builders.Email.build({:ok, entity}, email)
+      email -> BuilderProperties.build({:ok, entity}, Email, :email, email)
     end
   end
 
@@ -44,7 +49,7 @@ defmodule Core.User.Editor do
   defp name({:ok, entity}, name) do
     case name do
       nil -> {:ok, entity}
-      name -> Core.User.Builders.Name.build({:ok, entity}, :name, name)
+      name -> BuilderProperties.build({:ok, entity}, Name, :name, name)
     end
   end
 
@@ -55,7 +60,7 @@ defmodule Core.User.Editor do
   defp surname({:ok, entity}, surname) do
     case surname do
       nil -> {:ok, entity}
-      surname -> Core.User.Builders.Name.build({:ok, entity}, :surname, surname)
+      surname -> BuilderProperties.build({:ok, entity}, Name, :surname, surname)
     end
   end
 
